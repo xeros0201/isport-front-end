d8ed4ee8ed6c9020901bd228831c303d89af96fc
Tyler Beutel <tyler.beutel@blackbook.ai>
2023-02-21 12:13:36 +1000

    Table and LeagueTable, added Logo to Header/Footer

---
+++ src/components/layout/Table/Table.tsx
@@ -0,0 +1,33 @@
+import classNames from "classnames";
+import { useMemo } from "react";
+import "./Table.scss"
+
+interface TableProps {
+    children: React.ReactNode | React.ReactNode[];
+    compact?: boolean;
+    striped?: boolean;
+    hasFirstColumn?: boolean;
+}
+
+const Table = ({
+    children,
+    compact = false,
+    striped = false,
+    hasFirstColumn = false,
+}: TableProps) => {
+
+    const tableClasses = useMemo(() => classNames({
+        'table': true,
+        'table--compact': compact,
+        'table--striped': striped,
+        'table--has-first-column': hasFirstColumn,
+    }), [compact, striped, hasFirstColumn]);
+
+    return (
+        <table className={tableClasses}>
+            {children}
+        </table>
+    );
+};
+
+export default Table;
--- src/pages/admin/Test/Test.tsx
+++ src/pages/admin/Test/Test.tsx
@@ -1,9 +1,10 @@
+import { LeagueDropdown } from "../../../components/dropdowns";
 import { Page } from "../../../components/layout";
 
 const Test = () => {
     return (
         <Page title="Test">
-            <h1>Test page</h1>
+            <LeagueDropdown onChange={() => {}} />
         </Page>
     );
 }
