36c748b3cf62504eefa33e3259cbf015cfde6c59
Tri Le <haitridev@gmail.com>
2023-02-22 15:38:05 +0700

    Add ImageInput into Test Page

--- src/pages/admin/Test/Test.tsx
+++ src/pages/admin/Test/Test.tsx
@@ -1,11 +1,22 @@
 import { Page } from "../../../components/layout";
+import { ImageListType } from "react-images-uploading";
+import ImageInput from "../../../components/input/ImageInput/ImageInput";
+import { useState } from "react";
 
 const Test = () => {
-    return (
-        <Page title="Test">
-            <h1>Test page</h1>
-        </Page>
-    );
-}
+    const [images, setImages] = useState<ImageListType>([]);
+
+  return (
+    <Page title="Test">
+      <h1>Test page</h1>
+      <ImageInput
+        values={images}
+        onChange={(values: ImageListType): void => {
+          setImages(values);
+        }}
+      />
+    </Page>
+  );
+};
 
 export default Test;
