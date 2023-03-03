2baf73e4fb82cc50e4ce2eef9de5bdd7d8d32f03
Tyler Beutel <tyler.beutel@blackbook.ai>
2023-03-03 14:14:10 +1000

    Add labels to Logos

--- src/components/common/Logo/Logo.tsx
+++ src/components/common/Logo/Logo.tsx
@@ -7,6 +7,7 @@ type LogoProps = {
    */
   url: string;
   /**
+   * @optional
    * Image's height. default height is 32px
    */
   height?: number;
@@ -21,6 +22,11 @@ type LogoProps = {
    also align center.
    */
   isSquare?: boolean;
+  /**
+   * @optional
+   * Display a label next to the logo.
+   */
+  label?: string;
 };
 
 const Logo = ({
@@ -28,10 +34,12 @@ const Logo = ({
   height = 64,
   alt = "img",
   isSquare = false,
+  label
 }: LogoProps) => {
   const logoClasses = classNames({
     "logo": true,
-    "logo--square": isSquare
+    "logo--square": isSquare,
+    "logo--with-label": label,
   });
 
   const logoStyles: React.CSSProperties = {
@@ -42,10 +50,14 @@ const Logo = ({
   return (
     <div className={logoClasses} style={logoStyles} >
       <img
+        className="logo__img"
         src={url}
         height={height}
         alt={alt}
       />
+      {label && (
+        <p className="logo__label">{label}</p>
+      )}
     </div>
   );
 };
