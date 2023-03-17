621e4e636085c2f16ed58f182eba2b7e98ff6749
Chrisw <chris_white_12@hotmail.com>
2023-03-17 13:35:57 +1000

    basic layout

--- src/pages/public/Fixtures/Fixtures.tsx
+++ src/pages/public/Fixtures/Fixtures.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Button } from "../../../components/common";
 import { RoundFilter } from "../../../components/filters";
 import { Page } from "../../../components/layout";
 import useSearchParamsState from "../../../hooks/useSearchParamsState";
@@ -6,6 +8,8 @@ const Fixtures = () => {
   const [leagueId, setLeagueId] = useSearchParamsState("leagueId", "");
   const [seasonId, setSeasonId] = useSearchParamsState("seasonId", "");
   const [round, setRound] = useSearchParamsState("round", "");
+
+  const [selectedTab, setSelectedTab] = useState(0);
 
   return (
     <Page title="Fixtures">
@@ -18,6 +22,62 @@ const Fixtures = () => {
         onRoundChange={setRound}
       />
       <h1>Fixtures</h1>
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <Button
+          label="Match Overview"
+          onClick={() => { setSelectedTab(0) }}
+          type={selectedTab === 0 ? 'primary' : 'transparent'}
+
+        />
+        <Button
+          label="Match Statistics"
+          onClick={() => { setSelectedTab(1) }}
+          type={selectedTab === 1 ? 'primary' : 'transparent'}
+        />
+        <Button
+          label="Game Leaders"
+          onClick={() => { setSelectedTab(2) }}
+          type={selectedTab === 2 ? 'primary' : 'transparent'}
+        />
+      </div>
+      <div>
+        <div
+          style={{
+            borderRadius: '10px',
+            boxShadow: '3px 3px grey',
+            display: selectedTab === 0 ? 'flex' : 'none',
+            border: '1px solid black',
+            padding: '2rem',
+            marginTop: '1rem'
+          }}
+        >
+          Tab 1
+        </div>
+        <div
+          style={{
+            borderRadius: '10px',
+            boxShadow: '3px 3px grey',
+            display: selectedTab === 1 ? 'flex' : 'none',
+            border: '1px solid black',
+            padding: '2rem',
+            marginTop: '1rem'
+          }}
+        >
+          Tab 2
+        </div>
+        <div
+          style={{
+            borderRadius: '10px',
+            boxShadow: '3px 3px grey',
+            display: selectedTab === 2 ? 'flex' : 'none',
+            border: '1px solid black',
+            padding: '2rem',
+            marginTop: '1rem'
+          }}
+        >
+          Tab 3
+        </div>
+      </div>
     </Page>
   );
 };
