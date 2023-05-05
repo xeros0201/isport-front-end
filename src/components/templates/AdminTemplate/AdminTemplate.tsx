17b40d5d04b727fb8f957ca54119d7db277ef90a
Tyler Beutel <tyler.beutel@blackbook.ai>
2023-05-05 15:01:01 +1000

    Fix formatting and and role check on AdminTemplate

--- src/components/templates/AdminTemplate/AdminTemplate.tsx
+++ src/components/templates/AdminTemplate/AdminTemplate.tsx
@@ -7,37 +7,28 @@ import { checkAuth } from "../../../api/auth";
 const adminPrefix = import.meta.env.VITE_ADMIN_PREFIX;
 
 const staffMenu: Menu = [
-  { label: 'Leagues', path: `${adminPrefix}/leagues` },
-  { label: 'Seasons', path: `${adminPrefix}/seasons` },
-  { label: 'Teams', path: `${adminPrefix}/teams` },
-  { label: 'Players', path: `${adminPrefix}/players` },
-  { label: 'Matches', path: `${adminPrefix}/matches` },
-]
+  { label: "Leagues", path: `${adminPrefix}/leagues` },
+  { label: "Seasons", path: `${adminPrefix}/seasons` },
+  { label: "Teams", path: `${adminPrefix}/teams` },
+  { label: "Players", path: `${adminPrefix}/players` },
+  { label: "Matches", path: `${adminPrefix}/matches` },
+];
 
 const adminMenu: Menu = [
-    ...staffMenu,
-    { label: 'Users', path: `${adminPrefix}/users` },
-]
+  ...staffMenu,
+  { label: "Users", path: `${adminPrefix}/users` },
+];
 
 const AdminTemplate = () => {
+  const { user } = useCheckAuth()
 
-  const { isLoading, data } = useQuery(
-    ['getUserData'],
-    async () =>checkAuth(),
-    {   cacheTime: 1,
-        refetchOnMount:"always",
-        refetchOnWindowFocus: false ,
-        staleTime:1000*60*60*24
-    }
-); 
-    
   return (
     <div>
-     <Header
-        menu={data?.user?.role === "ADMIN" ? adminMenu : staffMenu}
+      <Header
+        menu={user?.role === "ADMIN" ? adminMenu : staffMenu}
         collapseWidth={680}
       >
-        {!isLoading && <AccountDropdown />}
+        <AccountDropdown />
       </Header>
       <Container>
         <Outlet />
