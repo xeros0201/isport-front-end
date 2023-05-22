32fcbe7fec432adbb0a3677d58ce0c612dc1b532
Tyler Beutel <tyler.beutel@blackbook.ai>
2023-05-22 11:28:59 +1000

    Add Google Analytics

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,16 +2,18 @@ import { Routing } from ".";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter } from "react-router-dom";
-import { IconContext } from "react-icons";
+import { useGoogleAnalytics } from "../../hooks";
 
 function App() {
   const queryClient = new QueryClient();
 
+  useGoogleAnalytics();
+
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-          <Routing />
-          <ReactQueryDevtools initialIsOpen={false} />
+        <Routing />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </BrowserRouter>
   );
---
+++ src/hooks/useGoogleAnalytics.tsx
@@ -0,0 +1,7 @@
+import ReactGA from "react-ga4";
+
+const useGoogleAnalytics = () => {
+  ReactGA.initialize("G-KV4CW38GSH");
+};
+
+export default useGoogleAnalytics;
